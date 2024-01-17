6c66b217a238d268efe3bd0eb02732c69d7f85b3
Pablo Chitolina <pablo.chitolina@fauna.com>
2024-01-17 12:44:40 -0300

    Add TokenType enum for Fauna serialization

---
+++ common/src/main/java/com/fauna/common/enums/TokenType.java
@@ -0,0 +1,41 @@
+package com.fauna.common.enums;
+
+/**
+ * Enumeration representing token types for Fauna serialization.
+ */
+public enum TokenType {
+    NONE,
+
+    START_OBJECT,
+    END_OBJECT,
+
+    START_ARRAY,
+    END_ARRAY,
+
+    START_PAGE,
+    END_PAGE,
+
+    START_REF,
+    END_REF,
+
+    START_DOCUMENT,
+    END_DOCUMENT,
+
+    FIELD_NAME,
+
+    STRING,
+
+    INT,
+    LONG,
+    DOUBLE,
+
+    DATE,
+    TIME,
+
+    TRUE,
+    FALSE,
+
+    NULL,
+
+    MODULE
+}
