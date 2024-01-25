acf57b59efa99c0cdfe9e06d312d1c27b86e804d
Pablo Chitolina <pablo.chitolina@fauna.com>
2024-01-25 14:24:46 -0300

    Resolved PR comments.

--- faunaJava/src/test/java/com/fauna/serialization/FaunaParserTest.java
+++ faunaJava/src/test/java/com/fauna/serialization/FaunaParserTest.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.fauna.common.enums.FaunaTokenType;
+import com.fauna.exception.SerializationException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -39,6 +41,22 @@ class FaunaParserTest {
         );
 
         assertReader(reader, expectedTokens);
+
+        String invalidJson = "{\"@int\": \"abc\"}";
+        InputStream invalidInputStream = new ByteArrayInputStream(invalidJson.getBytes());
+        FaunaParser invalidReader = new FaunaParser(invalidInputStream);
+
+        assertThrows(RuntimeException.class, invalidReader::getValueAsInt);
+    }
+
+    @Test
+    public void testUnexpectedEndDuringAdvance() throws IOException {
+
+        String json = "{\"@int\": \"123\"";
+        InputStream inputStream = new ByteArrayInputStream(json.getBytes());
+        FaunaParser reader = new FaunaParser(inputStream);
+
+        assertThrows(SerializationException.class, reader::read);
     }
 
     private static void assertReader(FaunaParser reader,
