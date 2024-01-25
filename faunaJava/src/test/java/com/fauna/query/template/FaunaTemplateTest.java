8df98d8016ed85f53b31ec2ab288bb6b29648b53
Pablo Chitolina <pablo.chitolina@fauna.com>
2024-01-25 17:58:27 -0300

    Removed failing test

--- faunaJava/src/test/java/com/fauna/query/template/FaunaTemplateTest.java
+++ faunaJava/src/test/java/com/fauna/query/template/FaunaTemplateTest.java
@@ -1,12 +1,9 @@
 package com.fauna.query.template;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
 class FaunaTemplateTest {
@@ -63,19 +60,6 @@ class FaunaTemplateTest {
         assertEquals(TemplatePartType.LITERAL, expanded.get(0).getType());
         assertEquals("{not_a_var}'", expanded.get(1).getPart());
         assertEquals(TemplatePartType.LITERAL, expanded.get(1).getType());
-    }
-
-    @Ignore
-    @Test
-    void testTemplates_WithUnsupportedIdentifiers() {
-        FaunaTemplate template = new FaunaTemplate("let x = ${かわいい}");
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            List<FaunaTemplate.TemplatePart> expanded = new ArrayList<>();
-            template.forEach(expanded::add);
-        });
-        String expectedMessage = "Invalid placeholder in template: line 1, col 9";
-        String actualMessage = exception.getMessage();
-        assertTrue(actualMessage.contains(expectedMessage));
     }
 
 }
