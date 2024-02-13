e85f3271c593b62f54bf6c107e894fa9ddc795a8
Pablo Chitolina <pablo.chitolina@fauna.com>
2024-02-12 21:26:07 -0300

    Code improvements

--- faunaJava/src/test/java/com/fauna/serialization/DeserializerTest.java
+++ faunaJava/src/test/java/com/fauna/serialization/DeserializerTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.fauna.common.types.Module;
 import com.fauna.exception.SerializationException;
-import com.google.common.reflect.TypeToken;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -35,7 +34,7 @@ public class DeserializerTest {
         return deserialize(reader, deserFunc);
     }
 
-    public static <T> T deserializeNullable(String str, TypeToken<T> targetType)
+    public static <T> T deserializeNullable(String str, Class<T> targetType)
         throws IOException {
         return deserialize(str, ctx -> Deserializer.generateNullable(ctx, targetType));
     }
@@ -59,39 +58,34 @@ public class DeserializerTest {
     @Test
     public void testDeserializeInt() throws IOException {
         int result = deserialize("{\"@int\":\"42\"}",
-            ctx -> Deserializer.generate(ctx, new TypeToken<>() {
-            }));
+            ctx -> Deserializer.generate(ctx, Integer.class));
         assertEquals(42, result);
     }
 
     @Test
     public void testDeserializeString() throws IOException {
         String result = deserialize("\"hello\"",
-            ctx -> Deserializer.generate(ctx, new TypeToken<>() {
-            }));
+            ctx -> Deserializer.generate(ctx, String.class));
         assertEquals("hello", result);
     }
 
     @Test
     public void deserializeNullable() throws IOException {
-        String result = deserializeNullable("null", new TypeToken<>() {
-        });
+        String result = deserializeNullable("null", String.class);
         assertNull(result);
     }
 
     @Test
     public void deserializeDate() throws IOException {
         LocalDate result = deserialize("{\"@date\": \"2023-12-03\"}",
-            ctx -> Deserializer.generate(ctx, new TypeToken<>() {
-            }));
+            ctx -> Deserializer.generate(ctx, LocalDate.class));
         assertEquals(LocalDate.of(2023, 12, 3), result);
     }
 
     @Test
     public void deserializeTime() throws IOException {
         Instant result = deserialize("{\"@time\": \"2024-01-23T13:33:10.300Z\"}",
-            ctx -> Deserializer.generate(ctx, new TypeToken<>() {
-            }));
+            ctx -> Deserializer.generate(ctx, Instant.class));
         Instant instant = Instant.parse("2024-01-23T13:33:10.300Z");
         assertEquals(instant, result);
     }
@@ -99,8 +93,7 @@ public class DeserializerTest {
     @Test
     public void deserializeTimeNoUTC() throws IOException {
         Instant result = deserialize("{\"@time\": \"2023-12-03T05:52:10.000001-09:00\"}",
-            ctx -> Deserializer.generate(ctx, new TypeToken<>() {
-            }));
+            ctx -> Deserializer.generate(ctx, Instant.class));
         Instant instant = Instant.parse("2023-12-03T05:52:10.000001-09:00");
         assertEquals(instant, result);
     }
@@ -108,16 +101,14 @@ public class DeserializerTest {
     @Test
     public void testDeserializeDouble() throws IOException {
         Double result = deserialize("{\"@double\":\"1.23\"}",
-            ctx -> Deserializer.generate(ctx, new TypeToken<>() {
-            }));
+            ctx -> Deserializer.generate(ctx, Double.class));
         assertEquals(1.23d, result);
     }
 
     @Test
     public void testDeserializeLong() throws IOException {
         Long result = deserialize("{\"@long\":\"123\"}",
-            ctx -> Deserializer.generate(ctx, new TypeToken<>() {
-            }));
+            ctx -> Deserializer.generate(ctx, Long.class));
         assertEquals(123l, result);
     }
 
@@ -125,8 +116,7 @@ public class DeserializerTest {
     public void testDeserializeBooleanTrue() throws IOException {
         InputStream inputStream = new ByteArrayInputStream("true".getBytes());
         Boolean result = deserialize(inputStream,
-            ctx -> Deserializer.generate(ctx, new TypeToken<>() {
-            }));
+            ctx -> Deserializer.generate(ctx, Boolean.class));
         assertTrue(result);
     }
 
@@ -134,16 +124,14 @@ public class DeserializerTest {
     public void testDeserializeBooleanFalse() throws IOException {
         InputStream inputStream = new ByteArrayInputStream("false".getBytes());
         Boolean result = deserialize(inputStream,
-            ctx -> Deserializer.generate(ctx, new TypeToken<>() {
-            }));
+            ctx -> Deserializer.generate(ctx, Boolean.class));
         assertFalse(result);
     }
 
     @Test
     public void testDeserializeLModule() throws IOException {
         Module result = deserialize("{\"@mod\": \"MyModule\"}",
-            ctx -> Deserializer.generate(ctx, new TypeToken<>() {
-            }));
+            ctx -> Deserializer.generate(ctx, Module.class));
         assertEquals(new Module("MyModule"), result);
     }
 
