728976bdf17b356a11d8247aab3c7104237bd3f7
David Griffin <findgriffin@gmail.com>
2024-07-15 12:08:44 -0700

    Update fauna/src/main/java/com/fauna/exception/QueryException.java
    Co-authored-by: Lucas Pedroza <40873230+pnwpedro@users.noreply.github.com>

--- fauna/src/main/java/com/fauna/exception/QueryException.java
+++ fauna/src/main/java/com/fauna/exception/QueryException.java
@@ -2,7 +2,7 @@ package com.fauna.exception;
 
 import com.fauna.response.QueryFailure;
 
-public class QueryException extends ServiceException {
+public class QueryRuntimeException extends ServiceException {
     public QueryException(QueryFailure response) {
         super(response);
     }
