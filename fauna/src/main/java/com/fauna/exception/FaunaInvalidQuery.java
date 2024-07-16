b81523cc90bc8a7ce6ee73049e86d0b1f7e2fefe
David Griffin <david.griffin@fauna.com>
2024-07-16 15:33:53 -0700

    Remove FaunaInvalidQuery.

--- fauna/src/main/java/com/fauna/exception/FaunaInvalidQuery.java
+++
@@ -1,9 +0,0 @@
-package com.fauna.exception;
-
-import com.fauna.response.QueryFailure;
-
-public class FaunaInvalidQuery extends ServiceException {
-    public FaunaInvalidQuery(QueryFailure response) {
-        super(response);
-    }
-}
