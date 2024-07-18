35f6416c6c28ddf030ef61de3c4e97f3595fc1fa
David Griffin <david.griffin@fauna.com>
2024-07-18 11:50:57 -0700

    Make RetryHandler compile.

--- fauna/src/main/java/com/fauna/client/RetryHandler.java
+++ fauna/src/main/java/com/fauna/client/RetryHandler.java
@@ -1,18 +1,50 @@
 package com.fauna.client;
 
-public class RetryHandler {
+import com.fauna.exception.FaunaException;
+import com.fauna.exception.RetryableException;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+
+public class RetryHandler<T> {
     private final RetryStrategy strategy;
     private final long startTimeMillis;
     private int requestCount = 0;
+    private final HttpClient client;
+    private final HttpRequest request;
 
 
-    public RetryHandler(RetryStrategy strategy) {
+    public RetryHandler(HttpClient client, HttpRequest request, RetryStrategy strategy) {
         this.strategy = strategy;
         this.startTimeMillis = System.currentTimeMillis();
+        this.client = client;
+        this.request = request;
     }
 
     public int getDelayMillis() {
         this.requestCount += 1;
         return this.strategy.getDelayMillis(startTimeMillis, requestCount);
     }
+
+    public CompletableFuture<HttpResponse<String>> send(HttpClient client, HttpRequest request) {
+        return client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    /**
+    public CompletableFuture<HttpResponse<String>> execute(HttpClient client, HttpRequest request) throws FaunaException, InterruptedException {
+        return this.send(client, request).exceptionally(exc -> {
+            if (exc instanceof RetryableException) {
+                Executor delayed = CompletableFuture.delayedExecutor(this.getDelayMillis(), TimeUnit.MILLISECONDS);
+                return CompletableFuture.supplyAsync(send(client, request));
+            } else {
+                throw new CompletionException(exc);
+            }
+        });
+    }
+     */
 }
--- fauna/src/test/java/com/fauna/client/TestRetryHandler.java
+++ fauna/src/test/java/com/fauna/client/TestRetryHandler.java
@@ -2,12 +2,18 @@ package com.fauna.client;
 
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestRetryHandler {
     @Test
     public void testDefaultHandler() {
-        RetryHandler handler = new RetryHandler(ExponentialBackoffStrategy.builder().build());
+        RetryHandler handler = new RetryHandler(HttpClient.newBuilder().build(),
+                HttpRequest.newBuilder().uri(URI.create("http://foo")).build(),
+                ExponentialBackoffStrategy.builder().build());
         assertEquals(1125.0, handler.getDelayMillis(), 125);
         assertEquals(2250.0, handler.getDelayMillis(), 250.0);
     }
