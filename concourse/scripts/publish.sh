eebeabdb1a8eb22848b9da0403752432323e3daa
Matt Freels <matt@freels.name>
2024-07-26 15:29:45 -0600

    update pipeline to use the 'release' branch, tag the release and fix docs pipeline

--- concourse/scripts/publish.sh
+++ concourse/scripts/publish.sh
@@ -2,11 +2,20 @@
 
 set -eou
 
+# setup
+
+echo 'setup git'
+export DEBIAN_FRONTEND=noninteractive
+apt-get -yqq update
+apt-get -yqq install git
+
 cd ./fauna-jvm-repository
 
-PACKAGE_VERSION=`grep version gradle.properties | cut -c 9-`
+PACKAGE_VERSION=$(./gradlew -q printVersion | tail -n 1)
 
-echo "Going to publish version: $PACKAGE_VERSION"
+# publish
+
+echo "publishing version: $PACKAGE_VERSION"
 
 export ORG_GRADLE_PROJECT_mavenCentralUsername="$MAVENCENTRAL_USER"
 export ORG_GRADLE_PROJECT_mavenCentralPassword="$MAVENCENTRAL_PASSWORD"
@@ -15,5 +24,16 @@ export ORG_GRADLE_PROJECT_signingInMemoryKeyId="$SIGNING_KEYID"
 export ORG_GRADLE_PROJECT_signingInMemoryKeyPassword="$SIGNING_KEYPASSWORD"
 
 ./gradlew publishToMavenCentral
+
+# release tag
+
+echo "tagging release-$PACKAGE_VERSION"
+cd ../
+git clone fauna-jvm-repository fauna-jvm-repository-tagged
+cd ./fauna-jvm-repository-tagged
+
+git config --global user.email "production@fauna.com"
+git config --global user.name "Fauna Engineering"
+git tag "release-$PACKAGE_VERSION"
 
 echo "fauna-jvm@$PACKAGE_VERSION has been published. (Can be delayed showing up in maven central.)" > ../slack-message/publish
