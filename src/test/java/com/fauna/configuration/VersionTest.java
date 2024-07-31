7c9791efe9ace9d2f8194f91492e53c2ef43d565
David Griffin <findgriffin@gmail.com>
2024-07-31 16:39:04 -0700

    [BT-5017-driver-env] Add tests to show that driver env is set correctly (#89)
    * Add DriverEnvironmentTest.
    
    * Get rid of warnings in RequestBuilderTest.java.
    
    ---------
    
    Co-authored-by: David Griffin <david.griffin@fauna.com>
    Co-authored-by: Matt Freels <matt@freels.name>

--- src/test/java/com/fauna/configuration/VersionTest.java
+++ src/test/java/com/fauna/configuration/VersionTest.java
@@ -4,14 +4,16 @@ import com.fauna.env.DriverEnvironment;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class VersionTest {
 
     @Test
     void getVersion_shouldReturnCorrectVersion() {
         String version = DriverEnvironment.getVersion();
-
-        assertEquals("1.0.0", version);
+        //
+        assertTrue(version.contains("0.1"));
+        assertTrue(version.contains("SNAPSHOT"));
     }
 
 }
