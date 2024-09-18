602db58a5db6af383ba9bb09dc92924fa49189aa
David Griffin <david.griffin@fauna.com>
2024-09-18 11:32:22 -0700

    Remove JsonCreator/JsonProperty from QueryStats.

--- src/main/java/com/fauna/response/QueryStats.java
+++ src/main/java/com/fauna/response/QueryStats.java
@@ -1,7 +1,5 @@
 package com.fauna.response;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fauna.constants.ResponseFields;
 import com.fauna.exception.ClientResponseException;
@@ -38,17 +36,8 @@ public final class QueryStats {
 
     private String stringValue = null;
 
-    @JsonCreator
-    public QueryStats(
-            @JsonProperty(ResponseFields.STATS_COMPUTE_OPS_FIELD_NAME) int computeOps,
-            @JsonProperty(ResponseFields.STATS_READ_OPS) int readOps,
-            @JsonProperty(ResponseFields.STATS_WRITE_OPS) int writeOps,
-            @JsonProperty(ResponseFields.STATS_QUERY_TIME_MS) int queryTimeMs,
-            @JsonProperty(ResponseFields.STATS_CONTENTION_RETRIES) int contentionRetries,
-            @JsonProperty(ResponseFields.STATS_STORAGE_BYTES_READ) int storageBytesRead,
-            @JsonProperty(ResponseFields.STATS_STORAGE_BYTES_WRITE) int storageBytesWrite,
-            @JsonProperty(ResponseFields.STATS_PROCESSING_TIME_MS) int processingTimeMs,
-            @JsonProperty(ResponseFields.STATS_RATE_LIMITS_HIT) List<String> rateLimitsHit) {
+    public QueryStats(int computeOps, int readOps, int writeOps, int queryTimeMs, int contentionRetries,
+                      int storageBytesRead, int storageBytesWrite, int processingTimeMs, List<String> rateLimitsHit) {
         this.computeOps = computeOps;
         this.readOps = readOps;
         this.writeOps = writeOps;
