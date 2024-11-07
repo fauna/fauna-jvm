8c7f27fc1ff8c6ecb126d2f5f09dd7b04305d820
Lucas Pedroza <lucas.pedroza@fauna.com>
2024-11-07 22:55:51 +0100

    Fix checkstyle issues in com.fauna.mapping.*

---
+++ src/main/java/com/fauna/mapping/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * The {@code com.fauna.mapping} package provides classes and utilities to support
+ * the mapping of data fields to and from Fauna, facilitating the serialization,
+ * deserialization, and formatting of fields used in Fauna data models.
+ *
+ * <p>The classes in this package include:
+ * <ul>
+ *   <li>{@link com.fauna.mapping.FieldInfo}: Holds metadata for individual fields, such as name,
+ *   type, and codec, used to map and handle fields within a Fauna data model.</li>
+ *
+ *   <li>{@link com.fauna.mapping.FieldName}: Provides utility methods for handling field names,
+ *   including a method to convert names to a canonical format.</li>
+ *
+ *   <li>{@link com.fauna.mapping.FieldType}: Defines various field types that can exist within
+ *   Fauna mappings, such as identifiers, timestamps, and general-purpose fields.</li>
+ * </ul>
+ */
+package com.fauna.mapping;
